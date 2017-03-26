0a138c2e602e96324db1bdf1678be16da9b1ab62
Mitchell Tague <tagumc13@wfu.edu>
2017-03-26 17:28:59 -0400

    Update and Remove Functionality

--- server.js
+++ server.js
@@ -75,15 +75,52 @@ app.post('/addItem', (req, res) => {
   console.log('got Post request');
   console.log(req.body);
 
+  req.body.quantity = Number(req.body.quantity);
+
   db.collection('Inventory').save(req.body, (err, result) => {
     if (err) {
 		    return console.log(err);
       }
     console.log('saved to database');
-    res.redirect('/shiftleader');
+    res.redirect('/shiftleader/inventory');
   })
 
 });
+
+app.post('/modify', (req, res) => {
+  console.log('got Post /update request');
+  console.log(req.body);
+
+  quant = Number(req.body.quantity);
+
+  console.log('Quant: ' + quant);
+
+  db.collection('Inventory').update(
+    {"item":req.body.item} ,
+    {$inc:{"quantity":quant},$set:{"date":req.body.date}} ,
+    (err, result) => {
+      if (err) {
+  		    return console.log(err);
+        }
+      console.log('saved to database');
+      res.redirect('/shiftleader/inventory');
+    })
+
+});
+
+app.post('/remove', (req, res) => {
+  console.log('got Post /update request');
+  console.log(req.body);
+  db.collection('Inventory').remove({"item":req.body.item},
+    (err, result) => {
+      if (err) {
+  		    return console.log(err);
+        }
+      console.log('saved to database');
+      res.redirect('/shiftleader/inventory');
+    })
+  });
+
 
 app.get('/home',(req,res)=>{
   res.sendFile(__dirname + '/title.html');
