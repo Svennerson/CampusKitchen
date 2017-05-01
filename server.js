a0080561ad3e431e7a837c4e05219a4c76f6f6a6
Mitchell Tague <tagumc13@wfu.edu>
2017-05-01 09:09:43 -0400

    Moving the mongodb back to the right place.

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ function responder(req, res) {
   console.log('got a request');
 }
 
-MongoClient.connect('mongodb://campuskitchen:123456@ds113660.mlab.com:13660/campuskitchen',
+MongoClient.connect('mongodb://campuskitchen:prohumanitate@ds113630.mlab.com:13630/campuskitchen',
 (err, database) => {
   if (err)
     return console.log(err);
